cb7ebcf289b9f80d2d499af766259ee56df045fe
Andreas Herz <a.herz@freegroup.de>
2016-03-31 17:46:09 +0200

    Updates

--- assets/shapes/draw2d_circuit_gate_AND.js
+++ assets/shapes/draw2d_circuit_gate_AND.js
@@ -2,7 +2,7 @@
 //                                                        
 // http://www.draw2d.org                                  
 //                                                        
-// Thu Mar 31 2016 17:19:41 GMT+0200 (CEST)                                         
+// Thu Mar 31 2016 17:46:05 GMT+0200 (CEST)                                         
 //                                                        
 // Go to the Designer http://www.draw2d.org               
 // to design your own shape or download user generated    
