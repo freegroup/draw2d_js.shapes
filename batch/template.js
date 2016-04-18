f8b11ca8cb3ece58f860eb1188c87bd7ca9aaf73
Herz, Andreas <andreas.herz@sap.com>
2016-04-18 10:04:28 +0200

    use naked draw2d.Canvas to render the generated code instead of the ShapeDesigner preview

--- batch/template.js
+++ batch/template.js
@@ -5,14 +5,50 @@ var img  = null;
 var reader = new draw2d.io.json.Reader();
 reader.unmarshal(app.view,json);
 
-
-new draw2d.io.png.Writer().marshal(app.view, function (imageDataUrl, base64) {
-    img = base64;
     var writer = new shape_designer.FigureWriter();
     writer.marshal(app.view, "testShape", function (js) {
         code = js;
+        try{
+            eval(js);
+        }
+        catch(exc){
+            alert("Error in shape code. \nRemove error and try it again:\n\n>>    "+exc);
+            throw exc;
+        }
+
+        var splash = $(
+            '<div class="overlay-scale">'+
+            '<div id="test_canvas">'+
+            '</div>'+
+            '<div>');
+
+        // fadeTo MUSS leider sein. Man kann mit raphael keine paper.text elemente einfügen
+        // wenn das canvas nicht sichtbar ist. In diesen Fall mach ich das Canvas "leicht" sichtbar und raphael ist
+        // zufrieden.
+        $("body").append(splash);
+        var canvas    = new draw2d.Canvas("test_canvas");
+        var test = new testShape();
+        canvas.add( test,400,160);
+        canvas.getBoundingBox= function(){
+            var xCoords = [];
+            var yCoords = [];
+            this.getFigures().each(function(i,f){
+                var b = f.getBoundingBox();
+                xCoords.push(b.x, b.x+b.w);
+                yCoords.push(b.y, b.y+b.h);
+            });
+            var minX   = Math.min.apply(Math, xCoords);
+            var minY   = Math.min.apply(Math, yCoords);
+            var width  = Math.max(10,Math.max.apply(Math, xCoords)-minX);
+            var height = Math.max(10,Math.max.apply(Math, yCoords)-minY);
+
+            return new draw2d.geo.Rectangle(minX,minY,width,height);
+        };
+
+        new draw2d.io.png.Writer().marshal(canvas, function (imageDataUrl, base64) {
+            img = base64;
+            splash.remove();
+        }, canvas.getBoundingBox().scale(10, 10));
     });
 
-
-}, app.view.getBoundingBox().scale(10, 10));
 
