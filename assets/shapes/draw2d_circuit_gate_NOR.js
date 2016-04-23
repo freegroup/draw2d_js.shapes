45fbae8a13e65d860268b442aad3beab912ffe60
Andreas Herz <a.herz@freegroup.de>
2016-04-23 21:27:21 +0000

    Updates

--- assets/shapes/draw2d_circuit_gate_NOR.js
+++ assets/shapes/draw2d_circuit_gate_NOR.js
@@ -11,20 +11,20 @@ var draw2d_circuit_gate_NOR = draw2d.SetFigure.extend({
 
    init:function(attr, setter, getter)
    {
-     this._super( $.extend({stroke:0, bgColor:null, width:57,height:61},attr), setter, getter);
+     this._super( $.extend({stroke:0, bgColor:null, width:40,height:40},attr), setter, getter);
      var port;
      // Port
-     port = this.createPort("input", new draw2d.layout.locator.XYRelPortLocator(0, 18.0327868852459));
+     port = this.createPort("input", new draw2d.layout.locator.XYRelPortLocator(0, 20));
      port.setConnectionDirection(3);
      port.setBackgroundColor("#1C9BAB");
      port.setName("Port");
      // Port
-     port = this.createPort("input", new draw2d.layout.locator.XYRelPortLocator(0, 83.60655737704919));
+     port = this.createPort("input", new draw2d.layout.locator.XYRelPortLocator(-2.5, 80));
      port.setConnectionDirection(3);
      port.setBackgroundColor("#1C9BAB");
      port.setName("Port");
      // Port
-     port = this.createPort("output", new draw2d.layout.locator.XYRelPortLocator(101.75438596491227, 50));
+     port = this.createPort("output", new draw2d.layout.locator.XYRelPortLocator(102.5, 51.25));
      port.setConnectionDirection(1);
      port.setBackgroundColor("#1C9BAB");
      port.setName("Port");
@@ -34,8 +34,8 @@ var draw2d_circuit_gate_NOR = draw2d.SetFigure.extend({
    createShapeElement : function()
    {
       var shape = this._super();
-      this.originalWidth = 57;
-      this.originalHeight= 61;
+      this.originalWidth = 40;
+      this.originalHeight= 40;
       return shape;
    },
 
@@ -44,23 +44,23 @@ var draw2d_circuit_gate_NOR = draw2d.SetFigure.extend({
        this.canvas.paper.setStart();
 
         // BoundingBox
-        shape = this.canvas.paper.path("M0,0 L57,0 L57,61 L0,61");
+        shape = this.canvas.paper.path("M0,0 L40,0 L40,40 L0,40");
         shape.attr({"stroke":"none","stroke-width":0,"fill":"none"});
         shape.data("name","BoundingBox");
         
         // Rectangle
-        shape = this.canvas.paper.path('M0,3Q0,0 3, 0L46,0Q49,0 49, 3L49,58Q49,61 46, 61L3,61Q0,61 0, 58L0,3');
+        shape = this.canvas.paper.path('M0,3Q0,0 3, 0L27,0Q30,0 30, 3L30,37Q30,40 27, 40L3,40Q0,40 0, 37L0,3');
         shape.attr({"stroke":"#303030","stroke-width":1,"fill":"#FFFFFF","opacity":1});
         shape.data("name","Rectangle");
         
         // Label
         shape = this.canvas.paper.text(0,0,'>1');
-        shape.attr({"x":12,"y":31,"text-anchor":"start","text":">1","font-family":"\"Arial\"","font-size":28,"stroke":"none","fill":"#080808","stroke-scale":true,"font-weight":"normal","stroke-width":0,"opacity":1});
+        shape.attr({"x":4,"y":21,"text-anchor":"start","text":">1","font-family":"\"Arial\"","font-size":20,"stroke":"none","fill":"#080808","stroke-scale":true,"font-weight":"normal","stroke-width":0,"opacity":1});
         shape.data("name","Label");
         
         // Circle
         shape = this.canvas.paper.ellipse();
-        shape.attr({"rx":6,"ry":6,"cx":51,"cy":30.5,"stroke":"#1B1B1B","stroke-width":1,"fill":"#FCFFFF","opacity":1});
+        shape.attr({"rx":6,"ry":6,"cx":34,"cy":20.5,"stroke":"#1B1B1B","stroke-width":1,"fill":"#FCFFFF","opacity":1});
         shape.data("name","Circle");
         
 
