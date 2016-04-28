23b1b9329933624b60916f95b9252425b7517724
Andreas Herz <a.herz@freegroup.de>
2016-04-28 11:17:47 +0000

    Updates

--- assets/shapes/draw2d_circuit_switch_HighLow.js
+++ assets/shapes/draw2d_circuit_switch_HighLow.js
@@ -142,6 +142,8 @@ draw2d_circuit_switch_HighLow = draw2d_circuit_switch_HighLow.extend({
          this._super(attr, setter, getter);
 
 
+        this.add(new draw2d.shape.basic.Label({text:"Label", x:40, y:20}), new draw2d.layout.locator.DraggableLocator());
+
         this.setResizeable(false);
         this.installEditPolicy(new draw2d.policy.figure.AntSelectionFeedbackPolicy());
         
